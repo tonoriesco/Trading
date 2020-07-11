e7621bbc04530b7ef2207e31b30d773b4786c7fa
Tono Riesco <tono.riesco@gmail.com>
2020-07-11 22:04:34 +0200

    Not anymore waiting for connection.

--- fill_historic_data_db.py
+++ fill_historic_data_db.py
@@ -14,9 +14,9 @@ import datetime
 # import yfinance as yf
 
 # pretty printing of pandas dataframe
-pd.set_option("expand_frame_repr", False)
+# pd.set_option("expand_frame_repr", False)
 # for pandas_datareader, sometimes there can be version mismatch
-pd.core.common.is_list_like = pd.api.types.is_list_like
+# pd.core.common.is_list_like = pd.api.types.is_list_like
 
 # ___variables___
 
@@ -32,18 +32,15 @@ start_time = datetime.datetime(2010, 1, 1)
 end_time = datetime.datetime.now().date().isoformat()  # today
 
 # get daily stock data from yahoo API
-connected = False
-while not connected:
-    try:
-        df = data.DataReader(
-            STOCK, start=start_time, end=end_time, data_source="yahoo"
-        )["Close"]
-        connected = True
-        # print(f"Connected to Yahoo\nGot {df} values\n")
-    except Exception as e:
-        print("type error: " + str(e))
-        # time.sleep(5)
-        pass
+try:
+    df = data.DataReader(STOCK, start=start_time, end=end_time, data_source="yahoo")[
+        "Close"
+    ]
+    connected = True
+    # print(f"Connected to Yahoo\nGot {df} values\n")
+except Exception as e:
+    print("type error: " + str(e))
+    pass
 
 
 # use numerical integer index instead of date
